d7461cf2019c2b22e7c87b97a3d29ee31d12951b
David Schoenholtz <dmschoenholtz@gmail.com>
2020-04-22 21:37:50 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,8 +1,5 @@
-# Make your shoe class here!
 class Shoe
-
-  attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+  attr_accessor :brand, :color, :size, :material, :condition
 
   def initialize(brand)
     @brand = brand
@@ -12,4 +9,5 @@ class Shoe
     puts "Your shoe is as good as new!"
     @condition = "new"
   end
+
 end
